b5d708444f05c9af794ce9b29574f48a67a6ea92
Zorka <jacobothegreat@gmail.com>
2025-01-26 13:26:37 -0700

    Gave guys to every level.  Made the sun moveable to give room for ass

--- scripts/sun.gd
+++ scripts/sun.gd
@@ -1,10 +1,12 @@
 extends Moon
 
+@export var hardcoded_position: bool = false
 var next_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position = get_viewport().get_visible_rect().size / 2
+	if !hardcoded_position:
+		position = get_viewport().get_visible_rect().size / 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
